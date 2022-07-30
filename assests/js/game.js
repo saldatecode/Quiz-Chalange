e9e32d952d254d8af35ec68fce9437ca2bc192e7
Codeword <saldatejared@gmail.com>
2022-07-30 07:59:33 -0700

    working on quiz page

---
+++ assests/js/game.js
@@ -0,0 +1,20 @@
+const question = document.querySelector('#question')
+const choices = array.from(document.querySelectorAll('.choice-text'))
+const progressText = document.querySelector('#progressText')
+const scoreText = document.querySelector('#scoreText')
+const progressBarFull = document.querySelector('#progressBarFull')
+
+let curreentQuestion = {}
+let acceptingAnswer = true
+let score = 0
+let questionCounter = 0
+let availableQuestion = []
+
+let questions = [
+    {
+        question: 'What is 2 + 2?',
+        choice:
+            choice:
+        choice:
+    }
+]
