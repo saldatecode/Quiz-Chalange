70fdc28fd996f2dedaabc9665beda1dce986a21d
Codeword <saldatejared@gmail.com>
2022-08-01 11:20:16 -0700

    function arrows ajusted

--- assests/js/game.js
+++ assests/js/game.js
@@ -67,63 +67,63 @@ const MAX_QUESTIONS = 4
 startGame = () => {
     questionCounter = 0
     score = 0
-    availableQuestion = [...questions]
+    availableQuestion = questions
     getNewQuestion()
 }
-function getNewQuestion() {
-    getNewQuestion = () => {
-        if (availableQuestion.length === 0 || questionCounter + 1 > MAX_QUESTIONS) {
-            localStorage.setItem('mostRecentScore', score)
 
-            return window.location.assign('/end.html')
-        }
+getNewQuestion = () => {
+    if (availableQuestion.length === 0 || questionCounter + 1 > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
 
-        questionCounter++
-        progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
-        progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`
-
-        const questionsIndex = Math.floor(Math.random() * availableQuestion.length)
-        currentQuestion = availableQuestion[questionsIndex]
-        question.innerText = currentQuestion.question
-
-        choices.forEach(choice => {
-            const number = choice.dataset['number']
-            choice.innerText = currentQuestion['choice' + number]
-        })
-
-        availableQuestion.splice(questionsIndex, 1)
-
-        acceptingAnswer = true
-
+        return window.location.assign('/end.html')
     }
+
+    questionCounter++
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`
+
+    const questionsIndex = Math.floor(Math.random() * availableQuestion.length)
+    currentQuestion = availableQuestion[questionsIndex]
+    question.innerText = currentQuestion.question
 
     choices.forEach(choice => {
-        choice.addEventListener('click', e => {
-            if (!acceptingAnswer) return
-
-            acceptingAnswer = false
-            const selectedChoice = e.target
-            const selectedAnswer = selectedChoice.dataset['number']
-
-            let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
-
-            if (classToApply === 'correct') {
-                incrementScore(SCORE_POINTS)
-            }
-
-            selectedChoice.parentElement.classList.add(classToApply)
-
-            setTimeout(() => {
-                selectedChoice.parentElement.classList.remove(classToApply)
-                getNewQuestion()
-
-            }, 1000)
-        })
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
     })
 
-    incrementScore = num => {
-        score += num
-        scoreText.innerText = score
-    }
+    availableQuestion.splice(questionsIndex, 1)
 
-    startGame()
+    acceptingAnswer = true
+
+}
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if (!acceptingAnswer) return
+
+        acceptingAnswer = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+
+        if (classToApply === 'correct') {
+            incrementScore(SCORE_POINTS)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+
+        }, 1000)
+    })
+})
+
+incrementScore = num => {
+    score += num
+    scoreText.innerText = score
+}
+
+startGame()
